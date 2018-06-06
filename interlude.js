401bf0f9f64d3c3e19ae9a439a3ac9d9bcede449
Joseph Keenan <keenan105@hotmail.com>
2018-06-06 03:11:36 +0000

    daily

--- interlude.js
+++ interlude.js
@@ -9,3 +9,4 @@ sayName();
 console.log(secret);
 
 // If you are inside of a function, you can look out and see data, but the opposite is NOT true. If you are outside of a function, you cannot look into a function.
+//
